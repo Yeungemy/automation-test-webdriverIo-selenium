873a146debf30c6d09c3b81ef65d55ae64e48a0a
Emy Yeung <emyzheng80@gmail.com>
2023-11-14 16:35:56 -0500

    updated

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -60,8 +60,46 @@ export const config: Options.Testrunner = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
-    }],
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            // to run chrome headless the following flags are required
+            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+            args: [
+                '--headless', 
+                '--disable-gpu',
+                '--start-maximized'
+            ],
+            }
+            //
+            // Parameter to ignore some or all default flags
+            // - if value is true: ignore all DevTools 'default flags' and Puppeteer 'default arguments'
+            // - if value is an array: DevTools filters given default arguments
+            // 'wdio:devtoolsOptions': {
+            //    ignoreDefaultArgs: true,
+            //    ignoreDefaultArgs: ['--disable-sync', '--disable-extensions'],
+            // }
+        }, {
+            // maxInstances can get overwritten per capability. So if you have an in house Selenium
+            // grid with only 5 firefox instance available you can make sure that not more than
+            // 5 instance gets started at a time.
+            maxInstances: 5,
+            browserName: 'firefox',
+            specs: [
+                './src/e2e/login/loginSuccess.spec.ts'
+            ],
+            'moz:firefoxOptions': {
+              // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+              args: ['-headless']
+            },
+            // If outputDir is provided WebdriverIO can capture driver session logs
+            // it is possible to configure which logTypes to exclude.
+            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+            excludeDriverLogs: ['bugreport', 'server'],
+            //
+            // Parameter to ignore some or all Puppeteer default arguments
+            // ignoreDefaultArgs: ['-foreground'], // set value to true to ignore all default arguments
+        }
+    ],
 
     //
     // ===================
