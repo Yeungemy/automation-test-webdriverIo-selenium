c38b244f3a6397a88ed73a6065ec4c4bb201684e
Emy Yeung <emyzheng80@gmail.com>
2023-11-25 18:36:30 -0500

    updated

--- src/pageobjects/auth/auth.page.ts
+++ src/pageobjects/auth/auth.page.ts
@@ -1,23 +1,4 @@
-// import Page from '../page';
-
 import Page from "../page";
-
-// class AuthPage extends Page {
-//     get selectors () {
-//         return {
-//            LOGIN_SUCCESSFULL_MSG : $('#content p')
-//         };
-//     }
-
-//     get strings (): any {
-//         return {
-//            LOGIN_SUCCESSFULL_MSG : 'Congratulations! You must have the proper credentials.'
-//         };
-//     }
-// }
-
-// const authPage = new AuthPage();
-// export {authPage};
 
 class AuthPage extends Page {
     /**
